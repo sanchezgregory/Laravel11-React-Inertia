cdfb7d46498fd6628010aeceae298a298a10a75c
Gregory Sanchez <mcgregox@gmail.com>
2024-07-16 19:17:07 -0300

    Fixing incompatible code between laravel and phpstan

--- app/Http/Resources/ProjectResource.php
+++ app/Http/Resources/ProjectResource.php
@@ -4,23 +4,14 @@ namespace App\Http\Resources;
 
 use App\Models\User;
 use Carbon\Carbon;
-use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProjectResource extends JsonResource
 {
-    /**
-     * @property int $id
-     * @property string $name
-     * @property string $description
-     * @property DateTime $created_at
-     * @property DateTime $due_date
-     * @property string $status
-     * @property string $image_path
-     * @property User $createdBy
-     * @property User $updatedBy
-     */
+    private mixed $updatedBy;
+    private mixed $createdBy;
+
     public function toArray(Request $request): array
     {
         $array = parent::toArray($request);
@@ -33,8 +24,8 @@ class ProjectResource extends JsonResource
             'due_date' => (new Carbon($array['due_date']))->format('Y-m-d'),
             'status' => $array['status'],
             'image_path' => $array['image_path'],
-            'created_by' => new UserResource($array['created_by']),
-            'updated_by' => new UserResource($array['updated_by']),
+            'created_by' => new UserResource($this->createdBy),
+            'updated_by' => new UserResource($this->updatedBy),
         ];
     }
 }
